939a9d4857d6cd64442fa8663438fecdf176b4d2
flsgk <harinjung.az@gmail.com>
2024-10-24 08:51:49 +0900

    📍링크 생성 버튼 코드 수정
    24.10.24 링크 생성 버튼 코드 수정

--- maker.js
+++ maker.js
@@ -2,12 +2,18 @@ createLink = () => {
   let linkButton = document.createElement("button");
   linkButton.textContent = "Check My WordSearch";
 
+  linkButton.classList.add("link-button");
+
   showLink = () => {
     window.location.href = "https://example.com"; // 나중에 path로 바꿔야 함!
   };
 
   linkButton.addEventListener("click", showLink);
 
+  let buttonWrapper = document.createElement("div");
+  buttonWrapper.classList.add("link-wrapper");
+  buttonWrapper.appendChild(linkButton);
+
   let submitDiv = document.querySelector(".submit");
-  submitDiv.appendChild(linkButton);
+  submitDiv.insertAdjacentElement("afterend", buttonWrapper);
 };
